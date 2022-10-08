71a6a604bd522da80026048d55405e413bb3e19d
CameronAron <cameron.aron04@gmail.com>
2022-10-08 01:23:37 -0500

    got a half working mouse movement script

---
+++ My project/Assets/GoToMousePointer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToMousePointer : MonoBehaviour
+{
+    public float moveSpeed = 9f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {      
+        transform.position = new Vector3(((Input.mousePosition.x - 550) / 30), (Input.mousePosition.y - 262) / 30, 0);
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    }
+}
