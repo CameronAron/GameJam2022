13a7f535bf5fb6d8e15212b22900f44db8583425
CameronAron <cameron.aron04@gmail.com>
2022-10-08 01:28:32 -0500

    changed name of project folder

--- 2022 Game Jam/Assets/GoToMousePointer.cs
+++ 2022 Game Jam/Assets/GoToMousePointer.cs
@@ -19,7 +19,7 @@ public class GoToMousePointer : MonoBehaviour
 
     void FixedUpdate()
     {      
-        transform.position = new Vector3(((Input.mousePosition.x - 550) / 30), (Input.mousePosition.y - 262) / 30, 0);
+        transform.position = new Vector3(((Input.mousePosition.x - 550) / 30), (Input.mousePosition.y - 262) / 30, -2);
         //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
     }
