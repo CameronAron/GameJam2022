a6f040097cb94f4e92b24120ebda13ef843b4ce9
CameronAron <cameron.aron04@gmail.com>
2022-10-08 11:31:13 -0500

    uh

--- 2022 Game Jam/Assets/GoToMousePointer.cs
+++ 2022 Game Jam/Assets/GoToMousePointer.cs
@@ -19,7 +19,7 @@ public class GoToMousePointer : MonoBehaviour
 
     void FixedUpdate()
     {      
-        transform.position = new Vector3(((Input.mousePosition.x - 550) / 30), (Input.mousePosition.y - 262) / 30, -2);
+        transform.position = new Vector3(((Input.mousePosition.x - 550) / 30), (Input.mousePosition.y - 262) / 30, -4);
         //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
     }
